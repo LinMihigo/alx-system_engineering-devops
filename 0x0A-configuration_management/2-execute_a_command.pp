ff65168aa678871fb8d84dde5df0d0ad239294dd
LinMihigo <linmihigo@gmail.com>
2025-01-11 14:21:15 +0200

    Task 2 - puppet-lint fixes

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 exec { 'pkill':
-  command  => 'pkill killmenow',
-  path     => ['/bin', '/usr/bin'],
-  onlyif   => 'pgrep -f killmenow',
+  command => 'pkill killmenow',
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => 'pgrep -f killmenow',
 }
