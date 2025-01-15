2a53dba0c89156ad67f3a8be44a4416b152638fd
LinMihigo <linmihigo@gmail.com>
2025-01-15 23:04:32 +0200

    Task 5

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,47 @@
+# Configuring nginx server with puppet
+
+exec { 'update_apt_cache':
+  command => '/usr/bin/apt-get update',
+  path    => ['/usr/bin', '/usr/sbin'],
+}
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['update_apt_cache'],
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => @("EOF")
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+    server_name _;
+
+    location /redirect_me {
+        return 301 https://linmihigo.github.io;
+    }
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+}
+EOF
+  ,
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => File['/etc/nginx/sites-available/default'],
+}
