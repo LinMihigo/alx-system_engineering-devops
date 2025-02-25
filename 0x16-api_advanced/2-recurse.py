437577cdadb72706a695dab4c53c28b32252e103
LinMihigo <linmihigo@gmail.com>
2025-02-25 21:41:28 +0200

    Task 2 pep8 fixes

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -12,7 +12,12 @@ def recurse(subreddit, hot_list=[], after=None):
     headers = {"User-Agent": "custom-script/1.0"}
     params = {"limit": 100, "after": after}
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False
+    )
 
     if response.status_code != 200:
         return None
