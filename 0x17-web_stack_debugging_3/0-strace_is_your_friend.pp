6caec9dfaf37d4583dcb32adc63a3c6a7d7e5101
LinMihigo <linmihigo@gmail.com>
2025-02-27 23:31:07 +0200

    Task 0 (6th fix)

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,18 @@
 # automated puppet fix (to find out why Apache is returning a 500 error)
 
-exec { 'Fix wordpress site':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider => shell,
+package { ['php', 'php-mysql', 'libapache2-mod-php']:
+    ensure => installed,
+}
+
+file { '/var/www/html':
+    ensure  => directory,
+    recurse => true,
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0755',
+}
+
+exec { 'restart_apache':
+    command     => '/etc/init.d/apache2 restart',
+    refreshonly => true,
 }
