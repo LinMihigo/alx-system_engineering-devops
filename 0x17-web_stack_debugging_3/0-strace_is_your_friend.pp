dbf0a31366598fd33f54a53999c8b49f91ca41b0
LinMihigo <linmihigo@gmail.com>
2025-02-27 20:45:59 +0200

    Task 0 (1st fix)

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # automated puppet fix (to find out why Apache is returning a 500 error)
 
-exec { 'Fix wordpress site':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider => shell,
+exec { 'fix-wordpress':
+  command => '/bin/chown -R www-data:www-data /var/www/html/wordpress && /bin/chmod -R 755 /var/www/html/wordpress',
+  path    => ['/bin', '/usr/bin'],
+  unless  => '/bin/test -d /var/www/html/wordpress',
 }
