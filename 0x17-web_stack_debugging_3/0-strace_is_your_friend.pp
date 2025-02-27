fab305ee75266f280fc0678c4b9673956fb9e244
LinMihigo <linmihigo@gmail.com>
2025-02-27 20:54:32 +0200

    Task 0 (3rd fix)

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,6 @@
 # automated puppet fix (to find out why Apache is returning a 500 error)
 
-file { '/etc/apache2/missing.conf':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => '# Automatically created by Puppet',
-}
-
-service { 'apache2':
-  ensure  => running,
-  enable  => true,
-  require => File['/etc/apache2/missing.conf'],
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
